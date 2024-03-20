e588f558f2679fb32904b740796e5ed568d86b6f
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-20 16:38:40 +0000

    zeus: Import Dolby Atmos blobs
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Icc9756e2d0646b60045f2456b3602683cd2a9dae

--- extract-files.sh
+++ extract-files.sh
@@ -17,6 +17,12 @@ function blob_fixup() {
         vendor/lib64/libcamximageformatutils.so)
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
+       vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
+       vendor/lib64/hw/audio.primary.taro.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
